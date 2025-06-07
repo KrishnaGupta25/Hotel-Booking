659aad64fe9fb745c6c0db2ac9a76bc042845cc1
KrishnaGupta25 <krishnagupta44382@gmail.com>
2025-06-07 23:08:54 +0530

    3rd commit

---
+++ server/controllers/roomController.js
@@ -0,0 +1,19 @@
+//API to create new room for hotel
+export const createRoom = async (req,res) => {
+
+}
+
+//API to get all rooms
+export const getRooms = async (req,res) => {
+    
+}
+
+//API to get all rooms for a specific hotel
+export const getOwnerRooms = async (req,res) => {
+    
+}
+
+//API to togglr availability of a room
+export const toggleRoomAvailability = async (req,res) => {
+    
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import connectDB from './configs/db.js';
 
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
+import userRouter from './routes/userRoutes.js';
+import hotelRouter from './routes/hotelRoutes.js';
 
 connectDB()
 const app = express()
@@ -18,5 +20,7 @@ app.use(clerkMiddleware())
 app.use("/api/clerk",clerkWebhooks);
 
 app.get('/',(req,res)=>res.send("API is working "))
+app.use('/api/user',userRouter)
+app.use('/api/hotels',hotelRouter)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
